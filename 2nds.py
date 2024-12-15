93b9da60264b7d0046df500b4116338eeb610dab
Miraj Bajracharya <mirajbajra@gmail.com>
2024-12-15 23:53:25 +0545

    emojis

--- 2nds.py
+++ 2nds.py
@@ -1,3 +1,35 @@
-list=[1,2,3,4,5 ]
-list.append(6)
-print(list)
+customer={
+    "name":"John",
+    "age":30,
+    "is_verified":True
+}
+print(customer["name"])
+print(customer.get("birthdate","1 jan 2023"))#if there is no birthdate it replaces it with the given value
+
+phn=input("Enter Phone Number: ")
+digit_mapping={
+    "1":"one",
+    "2":"two",
+    "3":"three",
+    "4":"four",
+    "5":"five",
+
+}
+output=""
+for ch in phn:
+    output += (digit_mapping.get(ch,"!"))+" "
+
+print(output)
+
+message=input(">")
+words=message.split(' ')
+emojis={
+    ":)":"[smiley]",
+    ":(":"[frownie]"
+
+}
+output=""
+for word in words:
+    output+=emojis.get(word,word)
+
+print(output)
