eb296ca3df4ef372f18ea14c9f1f328d76c803b9
Miraj Bajracharya <mirajbajra@gmail.com>
2024-12-18 12:52:16 +0545

    return functions

--- 2nds.py
+++ 2nds.py
@@ -1,35 +1,13 @@
-customer={
-    "name":"John",
-    "age":30,
-    "is_verified":True
-}
-print(customer["name"])
-print(customer.get("birthdate","1 jan 2023"))#if there is no birthdate it replaces it with the given value
-
-phn=input("Enter Phone Number: ")
-digit_mapping={
-    "1":"one",
-    "2":"two",
-    "3":"three",
-    "4":"four",
-    "5":"five",
-
-}
-output=""
-for ch in phn:
-    output += (digit_mapping.get(ch,"!"))+" "
-
-print(output)
-
+def emoji_converter(message):
+    words = message.split(' ')
+    emojis = {
+        ":)": "[smiley]",
+        ":(": "[frownie]",
+        "hii": "[hello]"
+    }
+    output = ""
+    for word in words:
+        output += emojis.get(word, word) + ' '
+    return output
 message=input(">")
-words=message.split(' ')
-emojis={
-    ":)":"[smiley]",
-    ":(":"[frownie]",
-    "hii":"[hello]"
-}
-output=""
-for word in words:
-    output+=emojis.get(word,word) + ' '
-
-print(output)
+print(emoji_converter(message))
