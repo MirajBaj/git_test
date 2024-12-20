f1a43464de02192df051891edd898c189b8f23b6
Miraj Bajracharya <mirajbajra@gmail.com>
2024-12-20 18:24:32 +0545

    directory

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
-import random
+from pathlib import Path
 
-class Dice:
-    def roll(self  ):
-        first= random.randint(1,6)
-        second=random.randint(1,6)
-        return (first,second)
-
-
-dice=Dice()
-print(dice.roll())
+path=Path()
+for file in path.glob('*'):
+    print(file)
