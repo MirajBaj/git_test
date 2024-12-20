b52970ef3b718ba85b90d3e25e37556cd3e5f63c
Miraj Bajracharya <mirajbajra@gmail.com>
2024-12-20 16:57:15 +0545

    dice

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
-from ecommerce import shipping
+import random
 
-shipping.calc_shipping()
+class Dice:
+    def roll(self  ):
+        first= random.randint(1,6)
+        second=random.randint(1,6)
+        return (first,second)
+
+
+dice=Dice()
+print(dice.roll())
